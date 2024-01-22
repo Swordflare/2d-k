624423153f4eff2613c0794fe9aa6e28bd3eb737
Swordflare <Flaristyt@gmail.com>
2024-01-22 20:35:37 +0200

    Screens

--- 2d/Assets/brr.cs
+++ 2d/Assets/brr.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class brr : MonoBehaviour
 {
@@ -11,6 +12,8 @@ public class brr : MonoBehaviour
     bool jump = false;
     bool crouch = false;
     public Transform size;
+    public Canvas gameOverText;
+    public Canvas winText;
     void Start(){
         runSpeed = 0f;
     }
@@ -26,6 +29,7 @@ public class brr : MonoBehaviour
         
         if (Input.GetButtonDown("Stop")){
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+            Time.timeScale = 1;
         }
     }
     public void OnGround(){
@@ -37,5 +41,15 @@ public class brr : MonoBehaviour
         
         controlller.Move(runSpeed * Time.fixedDeltaTime, crouch, jump);
         jump = false;
+    }
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.tag == "Water"){
+            gameOverText.gameObject.SetActive(true);
+            Time.timeScale = 0;
+        }
+        if(collision.tag == "Lasagna"){
+            winText.gameObject.SetActive(true);
+            Time.timeScale = 0;
+        }
     }
 }
--- 2d/Assets/gobrrrrrr.cs
+++ 2d/Assets/gobrrrrrr.cs
@@ -12,6 +12,7 @@ public class CharacterController2D : MonoBehaviour
 	[SerializeField] private Transform m_CeilingCheck;							// A position marking where to check for ceilings
 	[SerializeField] private Collider2D m_CrouchDisableCollider;				// A collider that will be disabled when crouching
 
+	
 	const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
 	private bool m_Grounded;            // Whether or not the player is grounded.
 	const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
@@ -129,6 +130,7 @@ public class CharacterController2D : MonoBehaviour
 			// Add a vertical force to the player.
 			m_Grounded = false;
 			m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+			
 		}
 	}
 
